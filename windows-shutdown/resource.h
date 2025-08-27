4d0f33b3fecfda49325e779a716aa1e82e6fe38e
aldia_mechdream <futami16237@gmail.com>
2025-08-27 18:28:39 +0800

    init

---
+++ windows-shutdown/resource.h
@@ -0,0 +1,30 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 windows-shutdown.rc 使用
+//
+#define IDD_WINDOWSSHUTDOWN_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSSHUTDOWN             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSSHUTDOWN             109
+#define IDR_MAINFRAME                   128
+#define IDB_PNG2                        132
+#define IDB_PNG1                        133
+#define IDB_SHUTDOWNPNG                 133
+#define IDC_STATIC                      -1
+#define IDB_RESTARTPNG                  65535
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
