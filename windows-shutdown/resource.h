fba5990c055956f9defdf5d2bc1cd6f379279c9b
aldia_mechdream <futami16237@gmail.com>
2025-08-31 14:47:47 +0800

    增加config图片

--- windows-shutdown/resource.h
+++ windows-shutdown/resource.h
@@ -13,6 +13,8 @@
 #define IDB_RESTARTPNG                  134
 #define IDI_ICON1                       135
 #define IDI_MAINICON                    135
+#define IDB_CONFIG                      136
+#define IDB_CONFIGPNG                   136
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,7 +22,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
