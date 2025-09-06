3d73124114ef02a31bb69a653eb2e09f26df2b32
aldia_mechdream <futami16237@gmail.com>
2025-09-06 14:27:42 +0800

    chore: 增加所有action的图标

--- windows-shutdown/resource.h
+++ windows-shutdown/resource.h
@@ -1,24 +1,29 @@
 //{{NO_DEPENDENCIES}}
 //
 
-#define IDS_APP_TITLE 103
+#define IDS_APP_TITLE       103
 #define IDC_WINDOWSSHUTDOWN 109
-#define IDB_SHUTDOWNPNG 133
-#define IDB_RESTARTPNG 134
-#define IDI_MAINICON 135
-#define IDB_CONFIGPNG 136
-#define IDB_LOCKPNG 138
-#define IDB_SLEEPPNG 139
-#define IDC_STATIC -1
+#define IDB_SHUTDOWNPNG     133
+#define IDB_RESTARTPNG      134
+#define IDB_CONFIGPNG       136
+#define IDB_LOCKPNG         138
+#define IDB_SLEEPPNG        139
+#define IDC_STATIC          -1
+
+// icons
+#define IDI_MAINICON    300
+#define IDI_RESTARTICON 301
+#define IDI_LOCKICON    302
+#define IDI_SLEEPICON   303
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC 1
+#define _APS_NO_MFC              1
 #define _APS_NEXT_RESOURCE_VALUE 140
-#define _APS_NEXT_COMMAND_VALUE 32771
-#define _APS_NEXT_CONTROL_VALUE 1000
-#define _APS_NEXT_SYMED_VALUE 110
+#define _APS_NEXT_COMMAND_VALUE  32771
+#define _APS_NEXT_CONTROL_VALUE  1000
+#define _APS_NEXT_SYMED_VALUE    110
 #endif
 #endif
